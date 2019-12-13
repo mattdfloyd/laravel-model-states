3e246f6bc6e3ead6ed2618c1300a2682ba4eb929
Brent Roose <brent@spatie.be>
2019-12-13 10:20:49 +0100

    Fix final state

--- tests/StateChangedEventTest.php
+++ tests/StateChangedEventTest.php
@@ -63,7 +63,7 @@ class StateChangedEventTest extends TestCase
         Event::assertDispatched(
             StateChanged::class,
             function (StateChanged $event) {
-                $this->assertNull($event->finalState);
+                $this->assertInstanceOf(StateB::class, $event->finalState);
 
                 return true;
             }
